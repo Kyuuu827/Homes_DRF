0f93558406caee10cd07949d269fa0f2b59665a0
Kyuuu827 <lck0827@gmail.com>
2022-03-19 23:21:14 +0900

    url 변경

--- products/urls.py
+++ products/urls.py
@@ -4,10 +4,10 @@ from rest_framework.routers import DefaultRouter
 from products import views
 
 
-# router = DefaultRouter(trailing_slash=False)
-# router.register(r'menus', views.MenuListViewSet, basename = 'MenuList')
+router = DefaultRouter(trailing_slash=False)
+router.register(r'menus', views.MenuListViewSet, basename = 'MenuList')
 
 urlpatterns = [
-    # path('', include(router.urls)),
-	path('menus', views.MenuListViewSet.as_view(), name = 'menu-list'), 
+    path('', include(router.urls)),
+	#path('menus', views.MenuListViewSet.as_view(), name = 'menu-list'), 
 ]
