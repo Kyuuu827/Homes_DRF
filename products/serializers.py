a7a99e6ca3b91914b443679fdfe2598c9872ceed
Kyuuu827 <lck0827@gmail.com>
2022-03-27 00:01:11 +0900

    serializer 수정

--- products/serializers.py
+++ products/serializers.py
@@ -1,16 +1,29 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from rest_framework.fields      import CharField
+
 from .models        import *
 
 
-class SubCategorySerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
+class SubCategorySerializer(ModelSerializer):
+    name = CharField(max_length=100)
 
-class CategorySerializer(serializers.Serializer):
-    name          = serializers.CharField(max_length=100)
-    subcategories = SubCategorySerializer(many=True)
+    class Meta:
+        model = SubCategory
+        fields = '__all__'
 
-class MenuSerializer(serializers.Serializer):
-    menu_name  = serializers.CharField(max_length=100)
-    image_url  = serializers.CharField(max_length=100)
-    categories = CategorySerializer(many=True)
+class CategorySerializer(SubCategorySerializer):
+    name = CharField(max_length=100)
+    subcategories = SubCategorySerializer(source='subcategory_set',many=True)
 
+    class Meta: 
+        model = Category
+        fields = 'name', 'subcategories'
+
+class MenuSerializer(CategorySerializer):
+    name       = CharField(max_length=100)
+    image_url  = CharField(max_length=100)
+    categories = CategorySerializer(source='category_set', many=True)
+
+    class Meta:
+        model = Menu
+        fields = 'name', 'image_url', 'categories'
