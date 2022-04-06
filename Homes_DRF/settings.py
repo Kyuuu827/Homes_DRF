bab1f0adcf2d3da9ba4eeba96b4c4303b7bfcdc1
Kyuuu827 <lck0827@gmail.com>
2022-04-07 00:01:36 +0900

    settings.py 수정

--- Homes_DRF/settings.py
+++ Homes_DRF/settings.py
@@ -139,7 +139,7 @@ APPEND_SLASH = False
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
-        # 'rest_flex_fields.filter.backends.FlexFieldsFilterBackend',
+        #'rest_flex_fields.filter.backends.FlexFieldsFilterBackend',
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
