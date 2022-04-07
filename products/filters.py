b833e5afc6994215307315d5d22c1abe67103161
Kyuuu827 <lck0827@gmail.com>
2022-04-08 00:39:28 +0900

    filter 기능추가

--- products/filters.py
+++ products/filters.py
@@ -1,10 +1,16 @@
 from django_filters import FilterSet, NumberFilter, OrderingFilter
 
+from products.models import ProductGroup
+
 
 class ProductGroupsFilter(FilterSet):
-    sub_category_id = NumberFilter()
+    sub_category_id = NumberFilter(field_name='subcategory_id', lookup_expr='exact')
     ordering = OrderingFilter(
         fields = (
-            
+
         )
     )
+
+    class Meta:
+        model = ProductGroup
+        fields = ''
