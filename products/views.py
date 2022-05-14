f5975ea684d76025c9105f942e0a35f00c7d7eb9
Kyuuu827 <lck0827@gmail.com>
2022-05-14 23:06:41 +0900

    products views

--- products/views.py
+++ products/views.py
@@ -3,7 +3,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets   import GenericViewSet
 from rest_framework.mixins     import ListModelMixin
 from rest_framework.response   import Response
-from rest_framework.decorators import action
 from products.defaultPagination import DefaultPagination
 
 from products.filters import ProductGroupFilter, ProductGroupsFilter
